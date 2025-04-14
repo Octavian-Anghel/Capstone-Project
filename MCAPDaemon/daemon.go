03cc19eff9bc5da004ff2e49a9b4b5281353a5e1
Ozwin Cordes <97770246+oz1x@users.noreply.github.com>
2025-04-14 19:03:21 -0400

    update go daemon code

--- MCAPDaemon/daemon.go
+++ MCAPDaemon/daemon.go
@@ -1,59 +1,76 @@
-/* 
-Golang Mcap file watcher daemon by Ozwin Cordes
-4/13/2025
-
-Copyright © 2012 The Go Authors. All rights reserved.
-Copyright © fsnotify Authors. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice, this
-  list of conditions and the following disclaimer in the documentation and/or
-  other materials provided with the distribution.
-* Neither the name of Google Inc. nor the names of its contributors may be used
-  to endorse or promote products derived from this software without specific
-  prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 package main
 
 import (
+	"errors"
+	"fmt"
 	"math"
+	"os"
+	"strings"
 	"sync"
 	"time"
-	"fmt"
-	"os"
 	"github.com/fsnotify/fsnotify"
 )
 
+func printTime(format string, a ...interface{}) {
+		fmt.Printf("[%s] ", time.Now().Format(time.RFC3339))
+	fmt.Printf(format+"\n", a...)
+}
+
+func exit(format string, a ...interface{}) {
+		printTime(format, a...)
+	os.Exit(1)
+}
+
+func getMagicBytes(filePath string) (string, error) {
+		file, err := os.Open(filePath)
+	if err != nil {
+			return "", err
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+			return "", err
+	}
+	fileSize := fileInfo.Size()
+
+	if fileSize < 5 {
+			return "", errors.New("filesize too small, likely bad file")
+	}
+
+	start := fileSize - int64(7)
+	buff := make([]byte, 7)
+
+	_, err = file.ReadAt(buff, start)
+	if err != nil {
+			return "", err
+	}
+
+	return string(buff), nil
+}
+
 func dedupLoop(w *fsnotify.Watcher) {
-	var (
-		// Wait 100ms for new events; each new event resets the timer.
-		waitFor = 100 * time.Millisecond
-
-		// Keep track of the timers, as path → timer.
-		mu     sync.Mutex
-		timers = make(map[string]*time.Timer)
-
-		// Callback we run.
+		var (
+		waitFor    = 100 * time.Millisecond
+		mu         sync.Mutex
+		timers     = make(map[string]*time.Timer)
 		printEvent = func(e fsnotify.Event) {
-			printTime(e.String())
+				printTime("Detected event: %s", e)
 
-			// Don't need to remove the timer if you don't have a lot of files.
+			if strings.HasSuffix(e.Name, ".mcap") {
+					magic, err := getMagicBytes(e.Name)
+				if err != nil {
+						fmt.Printf("Failed to read magic bytes from %s: %v\n", e.Name, err)
+				} else {
+						fmt.Printf("Magic bytes from %s: %s\n", e.Name, magic)
+					if magic == "MCAP0\r\n" {
+							fmt.Println("Valid MCAP file detected! pushing over to the hash and upload daemon\n")
+						// Additional processing here
+							HashNUpload(e.Name)
+					}
+				}
+			}
+
 			mu.Lock()
 			delete(timers, e.Name)
 			mu.Unlock()
@@ -61,90 +78,54 @@ func dedupLoop(w *fsnotify.Watcher) {
 	)
 
 	for {
-		select {
-		// Read from Errors.
-		case err, ok := <-w.Errors:
-			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
-				return
+			select {
+			case err, ok := <-w.Errors:
+			if !ok {
+					return
 			}
 			printTime("ERROR: %s", err)
-		// Read from Events.
+
 		case e, ok := <-w.Events:
-			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
-				return
+			if !ok {
+					return
 			}
 
-			// We just want to watch for file creation, so ignore everything
-			// outside of Create and Write.
 			if !e.Has(fsnotify.Create) && !e.Has(fsnotify.Write) {
-				continue
+					continue
 			}
 
-			fmt.
-			// Get timer.
 			mu.Lock()
 			t, ok := timers[e.Name]
 			mu.Unlock()
 
-			// No timer yet, so create one.
 			if !ok {
-				t = time.AfterFunc(math.MaxInt64, func() { printEvent(e) })
+					t = time.AfterFunc(math.MaxInt64, func() { printEvent(e) })
 				t.Stop()
-
 				mu.Lock()
 				timers[e.Name] = t
 				mu.Unlock()
 			}
 
-			// Reset the timer for this path, so it will start from 100ms again.
 			t.Reset(waitFor)
 		}
 	}
 }
 
-func getMagicBytes(filePath string) (string, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return "", err
-	}
-	fileSize := fileInfo.Size()
-
-	if fileSize < 5 {
-		return "", errors.New("Filesize too small, likely bad file.")
-	}
-
-	start := fileSize - int64(5)
-	buff := make([]byte, 5)
-
-	_, err = file.ReadAt(buffer, start)
-	if err != nil {
-		return "", err
-	}
-
-	return string(buff), nil
-}
-
 func main() {
-	// Create a new watcher.
 		w, err := fsnotify.NewWatcher()
-		if err != nil {
+	if err != nil {
 			exit("creating a new watcher: %s", err)
-		}
-		defer w.Close()
-	
-		// Start listening for events.
-		go dedupLoop(w)
-		// this is the directory the watcher will check for file events - modify for where your MCAPs are going.
-		path = "/shared"
-		err = w.Add(path)
-		if err != nil {
+	}
+	defer w.Close()
+
+	go dedupLoop(w)
+
+	path := "/shared"
+	err = w.Add(path)
+	if err != nil {
 			exit("%q: %s", path, err)
-		}
-		
+	}
+
+	// Prevent main from exiting
+	select {}
 }
---
+++ MCAPDaemon/hashfuncs.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+	"sort"
+)
+
+const NUM_THREADS = 56
+
+type HashResult struct {
+	Index int
+	Hash string
+}
+
+func GetSHA(filename string, startIndex int64, bytesPerThread int64, index int, wg *sync.WaitGroup, hashResults chan<- HashResult) {
+	defer wg.Done()
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Move to the correct position
+	_, err = file.Seek(startIndex, 0)
+	if err != nil {
+		fmt.Println("Error seeking file:", err)
+		return
+	}
+
+	hasher := sha256.New()
+	buffer := make([]byte, 4096)
+	var totalRead int64
+
+	for totalRead < bytesPerThread {
+		n, err := file.Read(buffer)
+		if n > 0 {
+			hasher.Write(buffer[:n])
+			totalRead += int64(n)
+		}
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+	}
+
+	hash :=fmt.Sprintf("%x", hasher.Sum(nil))
+	hashResults <- HashResult{Index: index, Hash: hash}
+	//fmt.Println("Hashing complete for chunk starting at:", startIndex)
+}
+
+func HashNUpload(fn string) {
+	filename := fn
+	fileInfo, err := os.Stat(filename)
+	if err != nil {
+		fmt.Println("Error getting file info:", err)
+		return
+	}
+
+	fileSize := fileInfo.Size()
+	bytesPerThread := fileSize / NUM_THREADS
+
+	var wg sync.WaitGroup
+	hashResults := make(chan HashResult, NUM_THREADS)
+
+	for i := 0; i < NUM_THREADS; i++ {
+		startIndex := int64(i) * bytesPerThread
+		wg.Add(1)
+		go GetSHA(filename, startIndex, bytesPerThread, i, &wg, hashResults)
+	}
+
+	wg.Wait()
+	close(hashResults)
+
+	var results []HashResult
+
+	for hr := range hashResults {
+		results = append(results, hr)
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Index < results[j].Index
+	})
+
+	finalHash := ""
+	for _, r := range results {
+		finalHash += r.Hash
+	}
+
+	fmt.Printf("sending hash to the Gateway node! hash value:\n %s", finalHash)
+}
